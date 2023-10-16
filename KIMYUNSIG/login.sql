53900b667dc40e1e37a53ec13fc1754903282da2
kimyunsig <147125471+kimyunsig@users.noreply.github.com>
2023-10-16 16:15:01 +0900

    login.sql

--- KIMYUNSIG/login.sql
+++ KIMYUNSIG/login.sql
@@ -4,9 +4,6 @@ user_id INT NOT NULL,
 pass varchar(15),
 passwordcheck varchar(10) not null,
 name varchar(5) not null,
-birth datetime ,-- 생년월일
-gender varchar(1),  
-email varchar(20),
 phone varchar(20) not null,
  FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
