0c94199c049afaf6903488609aa9b00ff8832a37
motioncomc <106040889+motionco@users.noreply.github.com>
2023-10-17 16:53:14 +0900

    Add files via upload

--- KimSooHyun/src/pages/list/List.jsx
+++ KimSooHyun/src/pages/list/List.jsx
@@ -1,12 +1,88 @@
-import React from 'react'
+import "./list.css";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
+import SearchItem from "../../components/searchItem/SearchItem";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { format } from "date-fns";
+import { DateRange } from "react-date-range";
+
 
 const List = () => {
+    const location = useLocation();
+    const [destination, setDestination] = useState(location.state.destination);
+    const [date, setDate] = useState(location.state.date);
+    const [openDate, setOpenDate] = useState(false);
+    const [options, setOptions] = useState(location.state.options);
     return (
         <div>
             <Navbar />
             <Header type="list" />
+            <div className="listContainer">
+                <div className="listWrapper">
+                    <div className="listSearch">
+                        <h1 className="lsTitle">검색</h1>
+                        <div className="lsItem">
+                            <label>경기장</label>
+                            <input placeholder={destination} type="text" />
+                        </div>
+                        <div className="lsItem">
+                            <label>날짜</label>
+                            <span onClick={() => setOpenDate(!openDate)}>  {`${format(date[0].startDate, "yy년MM월dd일")}~${format(
+                                date[0].endDate,
+                                "yy년MM월dd일"
+                            )}`} </span>
+                            {openDate && (
+                                <DateRange
+                                    onChange={(item) => setDate([item.selection])}
+                                    minDate={new Date()}
+                                    ranges={date}
+                                />
+                            )}
+
+                        </div>
+                        <div className="lsItem">
+                            <label>선택사항</label>
+                            <div className="lsOptions">
+                                <div className="lsOptionItem">
+                                    <span className="lsOptionText">
+                                        최저 가격
+                                    </span>
+                                    <input type="number" className="lsOptionInput" />
+                                </div>
+                                <div className="lsOptionItem">
+                                    <span className="lsOptionText">
+                                        최대 가격
+                                    </span>
+                                    <input type="number" className="lsOptionInput" />
+                                </div>
+                                <div className="lsOptionItem">
+                                    <span className="lsOptionText">인원</span>
+                                    <input
+                                        type="number"
+                                        min={1}
+                                        className="lsOptionInput"
+                                        placeholder={options.people}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <button>검색</button>
+                    </div>
+                    <div className="listResult">
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
