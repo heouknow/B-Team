286ccaa7cc0038876400c468ce65e8577c5a6501
heouknow <106498779+heouknow@users.noreply.github.com>
2023-10-17 10:03:37 +0900

    Add user table

---
+++ HeoYoonHo/user.sql
@@ -0,0 +1,11 @@
+CREATE TABLE B_Team.login (
+	user_Num INT auto_increment NOT NULL,
+	user_id varchar(100) NULL,
+	user_pw varchar(100) NULL,
+	user_pwcheck varchar(100) NULL,
+	user_Name varchar(100) NULL,
+	user_Phone varchar(100) NULL
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf32
+COLLATE=utf32_unicode_ci;
