fd07b119e8fe35cdf96a62f8062bf489e0c58ba3
motioncomc <106040889+motionco@users.noreply.github.com>
2023-10-18 16:08:54 +0900

    Add files via upload

--- KimSooHyun/src/App.js
+++ KimSooHyun/src/App.js
@@ -6,6 +6,7 @@ import {
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
 import Ground from "./pages/ground/Ground";
+import SignUp from "./pages/signup/SignUp";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/ground" element={<List />} />
         <Route path="/ground/:id" element={<Ground />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
