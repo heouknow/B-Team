6d7f49f6bdb320a355e90a367942919090de1dd1
motioncomc <106040889+motionco@users.noreply.github.com>
2023-10-20 10:19:21 +0900

    Add files via upload

--- KimSooHyun/src/components/navbar/Navbar.jsx
+++ KimSooHyun/src/components/navbar/Navbar.jsx
@@ -1,13 +1,24 @@
 import './navbar.css'
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const signUpGo = () => {
+        navigate("/signup");
+    };
+
+    const logIn = () => {
+        navigate("/login");
+    };
     return (
         <div className='navbar'>
             <div className="navContainer">
                 <span className="logo">Add in ground</span>
                 <div className="navItems">
-                    <button className="navButton">회원가입</button>
-                    <button className="navButton">로그인</button>
+                    <button className="navButton" onClick={signUpGo}>회원가입</button>
+                    <button className="navButton" onClick={logIn}>로그인</button>
                 </div>
             </div>
         </div>
